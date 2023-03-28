06e9c5b8a1e68e55bd9fc8bdbd01d329e214a07c
Etudiant O'clock <mike.dutheil@gmail.com>
2023-03-28 12:02:53 +0200

    pg-connect enlever

--- src/middlewares/sessionMiddleware.js
+++ src/middlewares/sessionMiddleware.js
@@ -1,16 +1,10 @@
 const session = require("express-session");
-const pgSession = require('connect-pg-simple')(session);
 const { Client } = require("pg"); 
 
 const client = new Client(process.env.PG_URL);
 client.connect();
 
 const sessionMiddleware = session({
-  store: new pgSession({
-    pool : client, 
-    tableName : 'user_session',
-    createTableIfMissing: true
-  }),
   secret: process.env.SESSION_SECRET, 
   resave: false, 
   saveUninitialized: true,
